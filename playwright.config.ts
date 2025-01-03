1b82b897644020ca92a0b17195840f12cdfe6c16
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2025-01-03 16:10:18 +0100

    feat(CI): switch E2E tests to playwright - WIP

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://127.0.0.1:4000',
+        baseURL: 'http://localhost:4000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry'
@@ -83,7 +83,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run test:simple-doc',
-        url: 'http://127.0.0.1:4000',
+        url: 'http://localhost:4000',
         reuseExistingServer: !process.env.CI
     }
 });
