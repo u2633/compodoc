427945cca02f1d783e83885ad631ccc76c25986b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2025-01-03 11:27:07 +0100

    fix(CI): E2E tests on Firefox

--- test/wdio/tests.js
+++ test/wdio/tests.js
@@ -1,14 +1,14 @@
 describe('Compodoc page', () => {
     it('should have a search bar, and handle results', async () => {
         await browser.url('http://localhost:4000/?q=exampleInput');
-        await browser.pause(3000);
+        await browser.pause(5000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(1);
     });
 
     it('should have a search bar, and handle results empty', async () => {
         await browser.url('http://localhost:4000/?q=waza');
-        await browser.pause(3000);
+        await browser.pause(5000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(0);
     });
@@ -17,7 +17,7 @@ describe('Compodoc page', () => {
         const browserName = browser.capabilities.browserName;
         if (browserName === 'chrome') {
             await browser.url('http://localhost:4000/?q=waza');
-            await browser.pause(3000);
+            await browser.pause(5000);
             const $body = await $('body');
             const color = await $body.getCSSProperty('background-color');
             await expect(color.value).toEqual('rgba(33,33,33,1)');
@@ -26,7 +26,7 @@ describe('Compodoc page', () => {
 
     it('should open menu for specific page', async () => {
         await browser.url('http://localhost:4000/modules.html');
-        await browser.pause(3000);
+        await browser.pause(5000);
         const menuModulesItem = await $('.d-md-block.menu .menu-toggler');
         await expect(menuModulesItem).toHaveElementClass('linked');
     });
