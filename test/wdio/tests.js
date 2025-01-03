80aa7737805ff88f278d4af006c26dbe94811169
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2025-01-03 11:55:30 +0100

    fix(CI): E2E tests on Firefox

--- test/wdio/tests.js
+++ test/wdio/tests.js
@@ -1,6 +1,15 @@
 describe('Compodoc page', () => {
     it('should have a search bar, and handle results', async () => {
         await browser.url('http://localhost:4000/?q=exampleInput');
+        await browser.waitUntil(
+            async () => {
+                return await browser.execute(() => document.readyState === 'complete');
+            },
+            {
+                timeout: 10000,
+                timeoutMsg: `La page n'était pas chargée après 10s`
+            }
+        );
         await browser.pause(5000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(1);
@@ -8,6 +17,15 @@ describe('Compodoc page', () => {
 
     it('should have a search bar, and handle results empty', async () => {
         await browser.url('http://localhost:4000/?q=waza');
+        await browser.waitUntil(
+            async () => {
+                return await browser.execute(() => document.readyState === 'complete');
+            },
+            {
+                timeout: 10000,
+                timeoutMsg: `La page n'était pas chargée après 10s`
+            }
+        );
         await browser.pause(5000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(0);
@@ -17,6 +35,15 @@ describe('Compodoc page', () => {
         const browserName = browser.capabilities.browserName;
         if (browserName === 'chrome') {
             await browser.url('http://localhost:4000/?q=waza');
+            await browser.waitUntil(
+                async () => {
+                    return await browser.execute(() => document.readyState === 'complete');
+                },
+                {
+                    timeout: 10000,
+                    timeoutMsg: `La page n'était pas chargée après 10s`
+                }
+            );
             await browser.pause(5000);
             const $body = await $('body');
             const color = await $body.getCSSProperty('background-color');
@@ -26,6 +53,15 @@ describe('Compodoc page', () => {
 
     it('should open menu for specific page', async () => {
         await browser.url('http://localhost:4000/modules.html');
+        await browser.waitUntil(
+            async () => {
+                return await browser.execute(() => document.readyState === 'complete');
+            },
+            {
+                timeout: 10000,
+                timeoutMsg: `La page n'était pas chargée après 10s`
+            }
+        );
         await browser.pause(5000);
         const menuModulesItem = await $('.d-md-block.menu .menu-toggler');
         await expect(menuModulesItem).toHaveElementClass('linked');
